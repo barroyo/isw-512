ae4f90a52141989585bb8b3cc32ad81341b77922
Bladimir Arroyo <bladimir.ab@gmail.com>
2018-11-05 20:38:23 -0600

    Adding localstorage to basic example

--- javascript/basicExample/js/application.js
+++ javascript/basicExample/js/application.js
@@ -1,13 +1,15 @@
 function validateUser() {
 	var username = document.getElementById('username').value;
 	var password = document.getElementById('password').value;
-	var errorElement = document.getElementById('error_msg')
+	var errorElement = document.getElementById('error_msg');
 
 	if (username == 'admin' && password == 'password') {
-		console.log('logged in')
-		errorElement.setAttribute("style","display:none;");
+		console.log('logged in');
+		localStorage.setItem('username', username);
+		errorElement.innerHTML = 'User is valid';
+		errorElement.setAttribute("style","display:block;");
 	} else {
 		errorElement.innerHTML = 'Username or Password invalid';
 		errorElement.setAttribute("style","display:block;");
-	}	
+	}
 }
