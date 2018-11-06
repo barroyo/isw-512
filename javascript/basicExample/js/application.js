66e097e137dd878133b9efe4e81a419b8311ddfe
Bladimir Arroyo <bladimir.ab@gmail.com>
2018-11-05 19:55:45 -0600

    Include basic event and DOM manipulation example

---
+++ javascript/basicExample/js/application.js
@@ -0,0 +1,13 @@
+function validateUser() {
+	var username = document.getElementById('username').value;
+	var password = document.getElementById('password').value;
+	var errorElement = document.getElementById('error_msg')
+
+	if (username == 'admin' && password == 'password') {
+		console.log('logged in')
+		errorElement.setAttribute("style","display:none;");
+	} else {
+		errorElement.innerHTML = 'Username or Password invalid';
+		errorElement.setAttribute("style","display:block;");
+	}	
+}
