8ad050ed494a83d5541faf65a7a6581c3878b5ac
Bladimir Arroyo <bladimir.ab@gmail.com>
2018-11-12 19:57:04 -0600

    Include bind events and localstorage basic functions

--- javascript/basicExample/js/application.js
+++ javascript/basicExample/js/application.js
@@ -7,9 +7,45 @@ function validateUser() {
 		console.log('logged in');
 		localStorage.setItem('username', username);
 		errorElement.innerHTML = 'User is valid';
-		errorElement.setAttribute("style","display:block;");
+		errorElement.setAttribute("style", "display:block;");
 	} else {
 		errorElement.innerHTML = 'Username or Password invalid';
-		errorElement.setAttribute("style","display:block;");
+		errorElement.setAttribute("style", "display:block;");
 	}
 }
+
+function saveUser() {
+	var username = document.getElementById('username').value;
+	var firstname = document.getElementById('firstname').value;
+	var password = document.getElementById('password').value;
+
+	var user = {
+		username,
+		firstname,
+		password
+	};
+
+	if (saveToLocalStorage('users', user)) {
+		console.log('User saved');
+	}
+}
+
+/**
+ * Binds the different events to the different elements of the page
+ */
+function bindEvents() {
+	// document.getElementById('login-button').addEventListener('click', loginButtonHandler);
+	// document.getElementById('register-button').addEventListener('click', registerButtonHandler);
+	jQuery('#login-button').bind('click', loginButtonHandler);
+	jQuery('#register-button').bind('click', registerButtonHandler);
+}
+
+function loginButtonHandler(element) {
+	validateUser();
+}
+
+function registerButtonHandler(element) {
+	saveUser();
+}
+
+bindEvents();
