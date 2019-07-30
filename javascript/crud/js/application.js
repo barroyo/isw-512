355085bbe2cd9d61f934eb75ae86c14709b3152f
Bladimir Arroyo <bladimir.ab@gmail.com>
2019-07-29 21:29:59 -0600

    Include edit for books

--- javascript/crud/js/application.js
+++ javascript/crud/js/application.js
@@ -1,16 +1,26 @@
 'use strict';
 
+const booksKey = 'books';
 var x = 20;
 
-function saveBook() {
+function insertBook() {
 	const title = document.getElementById('title').value;
 	const author = document.getElementById('author').value;
-	const booksKey = 'books';
+	let currentKey = localStorage.getItem('booksLastInsertedId');
+
+	if (!currentKey) {
+		localStorage.setItem('booksLastInsertedId', 1);
+		currentKey = 1;
+	} else {
+		currentKey = parseInt(currentKey) + 1;
+		localStorage.setItem('booksLastInsertedId', currentKey);
+	}
 
 	// create the book object
 	const book = {
 		title,
-		author
+		author,
+		id: currentKey
 	};
 
 	// add it to the database
@@ -22,6 +32,30 @@ function saveBook() {
 		books.push(book)
 	}
 	localStorage.setItem(booksKey, JSON.stringify(books));
+
+	clearFields();
+	// render the books
+	renderTable('books', books);
+}
+
+
+function saveBook() {
+	const title = document.getElementById('editTitle').value;
+	const author = document.getElementById('editAuthor').value;
+	const id = document.getElementById('editId').value;
+
+	// create the book object
+	const book = {
+		title,
+		author,
+		id
+	};
+	debugger;
+	// add it to the database
+	let books = JSON.parse(localStorage.getItem(booksKey));
+	let results = books.filter(book => book.id != id);
+	results.push(book);
+	localStorage.setItem(booksKey, JSON.stringify(results));
 
 	clearFields();
 	// render the books
@@ -54,8 +88,22 @@ function renderTable(tableName, tableData) {
 
 function editEntity(element) {
 	const dataObj = jQuery(element).data();
-	editElement(dataObj.entity, dataObj.id);
+
+	let books = JSON.parse(localStorage.getItem(booksKey));
+	let bookFound;
+	books.forEach(function (book) {
+		if (book.id == dataObj.id) {
+			bookFound = book;
+			return;
+		}
+	});
+
+	document.getElementById('editAuthor').value = bookFound.author;
+	document.getElementById('editTitle').value = bookFound.title;
+	document.getElementById('editId').value = bookFound.id;
 }
+
+
 
 function deleteEntity(element) {
 	const dataObj = jQuery(element).data();
@@ -73,6 +121,10 @@ function loadTableData(tableName) {
  */
 function bindEvents() {
 	jQuery('#add-book-button').bind('click', function (element) {
+		insertBook();
+	});
+
+	jQuery('#save-book-button').bind('click', function (element) {
 		saveBook();
 	});
 }
